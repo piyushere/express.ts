dcf650269f712f31cbee2dd1491989f914d8dc2e
piyushere <kingepiyush@gmail.com>
2024-01-30 13:09:16 +0530

    upgraded all the packages, fixed import resolver issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
@@ -16,9 +21,7 @@ module.exports = {
       'ignorePackages',
       {
         js: 'never',
-        jsx: 'never',
         ts: 'never',
-        tsx: 'never',
       },
     ],
     'import/no-extraneous-dependencies': 'off',
@@ -26,5 +29,10 @@ module.exports = {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-use-before-define': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
   },
 };
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
+const port = 8000;
 app.use(bodyParser.json());
 app.get('/ping', (req, res) => res.send({ message: 'hello world' }));
 app.post('/ping', (req, res) => res.send(req.body));
-app.listen(8000, () => console.log('application running on port 8000'));
+app.listen(port, () => console.log(`application running on port ${port}`));
